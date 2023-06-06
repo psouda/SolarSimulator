7a74e06052e42721e75525411dbd3064088b9231
PAYAM SOUDACHI <payams@energizeglobal.com>
2023-06-06 11:16:44 +0400

    Add custom exception classes

---
+++ src/main/java/com/solar/solarsimulator/exception/JsonFileIOException.java
@@ -0,0 +1,7 @@
+package com.solar.solarsimulator.exception;
+
+public class JsonFileIOException extends RuntimeException {
+  public JsonFileIOException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
---
+++ src/main/java/com/solar/solarsimulator/exception/JsonParsingException.java
@@ -0,0 +1,7 @@
+package com.solar.solarsimulator.exception;
+
+public class JsonParsingException extends RuntimeException {
+  public JsonParsingException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
